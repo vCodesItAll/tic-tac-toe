bd00201e523700c58fecb04dc61b7ae9c41c01c6
Vanessa Dickey <vcdickey@gmail.com>
2023-10-03 19:13:04 +0000

    expanded sandbox

--- js/sandbox.js
+++ js/sandbox.js
@@ -4,9 +4,21 @@
 // State
 // we want to build a domain model for state
 // Game Status
+    let gameState = {};
 // boardState
-    currentTurn = 0;
-// currentTurn = currentPlayers[currentTurn]; // player 
+    let tileObj = {};
+    let checkObj = {};
+    let entityObj = {title:tileObj, symbol:symbolObj}
+    let boardState = newArray(64).fill();
+    gameState.boardState = boardState();
+    let  currentTurn = 0;
+    gameState.currentTurn = currentTurn;
+    let currentPlayers = [];
+    currentPlayers.push({name: "Player 1"});
+    currentPlayers.push({name:"Player 2"});
+// currentTurn = currentPlayers[currentTurn]; // player 1
+    gameState.currentPlayers = currentPlayers;
+    console.log(gameState);
 // 
 // CurrentPlayers
 //player1 player2(CPU)
